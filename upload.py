995fd193d7122092e979eb5301b1565f217c5261
Robin Kraft <rkraft4@gmail.com>
2017-10-16 21:33:43 -0700

    Simpler uploading

--- upload.py
+++ upload.py
@@ -4,22 +4,18 @@ import sys
 from mapbox import Uploader
 
 USER = 'robinkraft'
-BASEPATH = '/data/saturday/out'
-FNAME_MODEL = '{}_wm.tif'
-DATE = '2017-10-14'
 
 if __name__ == '__main__':
-    mosaic_id = sys.argv[1]
+    path = sys.argv[1]
+    mosaic_id = sys.argv[2]
+    date = sys.argv[3]
 
-    JOBS = []
-
+    basepath, fname = os.path.split(path)
 
     u = Uploader()
 
-    fname = FNAME_MODEL.format(mosaic_id)
-    path = os.path.join(BASEPATH, fname)
-    tileset = '{}.{}-{}'.format(USER, mosaic_id, DATE)
+    tileset = '{}.{}-{}'.format(USER, mosaic_id, date)
     url = u.stage(open(path))
     job = u.create(url, tileset, name=fname).json()
+
     print(job)
-    JOBS.append(job)
